2297e8e0c548cb3bef25963bcb7dacd18fea0d14
Iker-Galante <ikergalant@gmail.com>
2025-10-30 10:44:33 -0300

    Use another collection name and db

--- app/db.py
+++ app/db.py
@@ -3,8 +3,8 @@ import redis
 
 def get_mongo_collection():
     client = MongoClient("mongodb://localhost:27017/")
-    db = client["my_database"]
-    return db["my_collection"]
+    db = client["tp_bd2"]
+    return db["aseguradoras"]
 
 def get_redis_client():
     return redis.StrictRedis(host="localhost", port=6379, db=0)
