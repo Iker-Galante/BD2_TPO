ca34cd41b34a8243661218bebd77c3b2353f04ca
Iker-Galante <ikergalant@gmail.com>
2025-10-30 11:59:55 -0300

    Now polizas is inside client

--- app/main.py
+++ app/main.py
@@ -4,17 +4,27 @@ from db import get_mongo_collection
 def load_csv_to_mongo():
     mongo_collection = get_mongo_collection()
     csv_files = [
-        "resources/agentes.csv",
         "resources/clientes.csv",
         "resources/polizas.csv",
         "resources/siniestros.csv",
+        "resources/agentes.csv",
         "resources/vehiculos.csv"
     ]
 
     for file in csv_files:
         df = pd.read_csv(file)
         records = df.to_dict(orient="records")
-        mongo_collection.insert_many(records)
+        if file == "resources/polizas.csv":
+            for record in records:
+                # Extract id_cliente for the query but remove it from the record
+                id_cliente = record["id_cliente"]
+                poliza_record = {k: v for k, v in record.items() if k != "id_cliente"} #Saco al id del cliente 
+                query_filter = {"id_cliente": id_cliente}
+                update_operation = {"$push": {"polizas": poliza_record}}
+                mongo_collection.update_one(query_filter, update_operation)
+        else :
+            mongo_collection.insert_many(records)
+
         print(f"Inserted {len(records)} records from {file}")
 
 if __name__ == "__main__":
